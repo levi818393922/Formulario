bca02581fd78ccea27bcb9decafab08000aed96e
levi818393922 <leviigabriel34@gmail..com>
2022-06-14 11:37:16 -0500

    arquivos alterados na aula

---
+++ conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+//Fazer uma conexão simples no banco de dados 
+$servidor = "localhost";
+$bdname = "cadastro";
+$usuario = "root";
+$senha = "";
+
+$conexao = mysqli_connect($sevidor, $usuario, $senha, $bdname);
+
+//Verifica se deu certo a conexão ocm o banco de dados
+if(!$conexao){
+    die("Problemas na conexão com o banco de dados, Descrição do problema:" . mysqli_connect_error());
+}
+ echo "Operação realizada com sucesso! <br>";
+ //echo "<br>";
+ echo "Obrigado por utilizar nosso sistema!";
+// Fecha a conexão BD
+mysqli_close($conexao);
+?>
